fe4d697ab1b86e616b2731362df25c17c81d227a
Jan Engelke <jan.engelke@gmx.net>
2019-12-27 17:02:02 +0100

    added bubble-sort

---
+++ 09_Sorting_BubbleSort.py
@@ -0,0 +1,16 @@
+# Complete the countSwaps function below.
+def countSwaps(a):
+    s = 0
+    for i in range(len(a)):
+        for j in range(len(a) - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                s += 1
+    print("Array is sorted in {} swaps.".format(s))
+    print("First Element:", a[0])
+    print("Last Element:", a[-1])
+
+if __name__ == '__main__':
+    n = int(input())
+    a = list(map(int, input().rstrip().split()))
+    countSwaps(a)
